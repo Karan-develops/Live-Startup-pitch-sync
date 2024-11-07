7a9ef1b6c42f9a444f1ae1ab3ae070edd59580b6
Karan <mrkaran2k5@gmail.com>
2024-11-07 16:30:57 +0530

    UI fixes

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,7 +5,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import markdownit from "markdown-it";
-import { RefreshCwOff } from "lucide-react";
+import {
+  ArrowBigDown,
+  CalendarArrowUp,
+  RefreshCwOff,
+  UserRound,
+} from "lucide-react";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
@@ -22,18 +27,21 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   return (
     <>
       <section className="pink_container !min-h-[230px] ">
-        <p className="tag">{formatDate(post?._createdAt)}</p>
+        <p className="flex gap-1 tag">
+          <CalendarArrowUp />
+          {formatDate(post?._createdAt)}
+        </p>
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading !max-w-5xl">{post.description}</p>
       </section>
-      <section className="section-container">
+      <section className="section_container">
         <img
           src={post.image}
           alt="startup_thumbnail"
-          className="size-32 rounded-xl"
+          className="image_startup"
         />
-        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
-          <div className="flex-between gap-5">
+        <div className="space-y-5 mt-10 max-w-5xl mx-auto">
+          <div className="flex-between gap-5 bg-gray-500 p-2 rounded-lg border-2 border-teal-500 shadow-md shadow-teal-400">
             <Link
               href={`user/${post.author?._id}`}
               className="flex gap-2 items-center mb-3"
@@ -46,18 +54,24 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 className="rounded-full drop-shadow-md"
               />
               <div>
-                <p className="text-20-medium">{post.author.name}</p>
-                <p className="text-16-medium !text-black-300">
+                <p className="flex text-24-black">
+                  <UserRound className="size-5 mt-[5px]" />
+                  {post.author.name}
+                </p>
+                <p className="text-16-medium !text-black-200">
                   @{post.author.username}
                 </p>
               </div>
             </Link>
             <p className="category-tag">{post.category}</p>
           </div>
-          <h3 className="text-30-bold">Pitch Details</h3>
+          <h3 className="flex text-30-bold !text-white">
+            <ArrowBigDown className="text-white size-8 mt-2" />
+            Pitch Details <ArrowBigDown className="text-white size-8 mt-2" />
+          </h3>
           {parsedContent ? (
             <article
-              className="prose max-w-4xl font-work-sans break-all"
+              className="prose prose-invert max-w-4xl font-work-sans break-all text-white"
               dangerouslySetInnerHTML={{ __html: parsedContent }}
             />
           ) : (
@@ -70,7 +84,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <hr className="divider" />
         {/* TODO: recommeded or editor selected startups */}
         <Suspense fallback={<Skeleton className="view-skeleton" />}>
-          <View id={id}/>
+          <View id={id} />
         </Suspense>
       </section>
     </>
--- components/Ping.tsx
+++ components/Ping.tsx
@@ -5,8 +5,8 @@ const Ping = () => {
     <div className="relative">
       <div className="absolute -left-4 top-1">
         <span className="flex size-[11px]">
-          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75"></span>
-          <span className="relative inline-flex size-[11px] rounded-full bg-primary"></span>
+          <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-600 opacity-75"></span>
+          <span className="relative inline-flex size-[11px] bg-red-600 rounded-full"></span>
         </span>
       </div>
     </div>
