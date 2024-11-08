da576a33dbe0cad551f5652c5a4a926b3c225dd5
Karan <mrkaran2k5@gmail.com>
2024-11-08 20:55:00 +0530

    Playlist feature

--- components/UserStartups.tsx
+++ components/UserStartups.tsx
@@ -7,7 +7,7 @@ const UserStartups = async ({ id }: { id: string }) => {
   const startups = await client.fetch(STARTUPS_BY_AUTHOR_QUERY, { id });
   return (
     <>
-      {startups.length > 0 ? (
+      {startups?.length > 0 ? (
         startups.map((startup: StartupTypeCard) => (
           <StartupCard key={startup._id} post={startup} />
         ))
